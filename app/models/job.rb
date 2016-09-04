d8d32c5a49800565c9d849a7bfff1f8343be2eee
xdite <xuite.joke@gmail.com>
2016-09-04 20:53:47 +0800

    image_url

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+  def image_url
+    "http://placehold.it/120x120?text=placeholder"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..5 do
+  Job.create!(title: "Job #{i}", content: "Good Job #{i}")
+end
